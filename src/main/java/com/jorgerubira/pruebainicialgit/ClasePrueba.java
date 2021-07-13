3e25e9719979f2f7c89a1ed22bd7d604fbfc8f0d
JorgeRubira <jorgesub@gmail.com>
2021-07-13 18:18:15 +0200

    Comentario Propio

--- src/main/java/com/jorgerubira/pruebainicialgit/ClasePrueba.java
+++ src/main/java/com/jorgerubira/pruebainicialgit/ClasePrueba.java
@@ -11,5 +11,8 @@ public class ClasePrueba {
     }
     public int resta(int a, int b){
         return a-b;
-    }  
+    } 
+    public int multiplica(int a, int b){
+        return a*b;
+    }     
 }
