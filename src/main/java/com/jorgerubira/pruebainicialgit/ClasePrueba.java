9538b8a528f1318a5c09f2d05c39abe2f29b6e29
JorgeRubira <jorgesub@gmail.com>
2021-07-13 17:03:02 +0200

    Comentario Propio

---
+++ src/main/java/com/jorgerubira/pruebainicialgit/ClasePrueba.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jorgerubira.pruebainicialgit;
+
+public class ClasePrueba {
+    public int suma(int a, int b){
+        return a+b;
+    }
+}
